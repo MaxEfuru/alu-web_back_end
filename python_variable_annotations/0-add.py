4df0e22a46d2f0f72cfa08898a3d94bd4c23db4c
Maximum Fredrick <102311122+MaxEfuru@users.noreply.github.com>
2024-09-06 22:47:27 +0000

    update

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
-    """Add two floats and return the result"""
+    """Add two annotated floats and return the result"""
     return a + b
